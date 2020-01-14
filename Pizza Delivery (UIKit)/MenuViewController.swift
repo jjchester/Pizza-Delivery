43a416f42d4915871eef39bcc3eb66d8dbfdd438
Justin Chester <jchester@git.socs.uoguelph.ca>
2020-01-14 17:16:21 -0500

    Add placeholder menu page

---
+++ Pizza Delivery (UIKit)/MenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MenuViewController.swift
+//  Pizza Delivery (UIKit)
+//
+//  Created by Justin Chester on 2020-01-14.
+//  Copyright © 2020 Justin Chester. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let greenColorCode = 0x69834d
+        //let beigeColorCode = 0xf2e9de
+        let greenColor = UIColor(rgb: greenColorCode)
+        self.view.backgroundColor = greenColor
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
